47b987496baa01bd93c30f8cd7b41b0c76d3f71b
naivedh <naivedh@gmail.com>
2025-01-23 17:12:05 +0530

    1

--- text.groovy
+++ text.groovy
@@ -4,16 +4,15 @@ podTemplate(
   showRawYaml: false,
   containers: [
     containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:latest', alwaysPullImage: true, privileged: true),
-    containerTemplate(name: 'all-in-one', image: 'naivedh/jenkins-agent-all-in-one:latest', command: 'cat', ttyEnabled: true)
+    containerTemplate(name: 'all-in-one', image: 'naivedh/jenkins-agent-all-in-one:latest',alwaysPullImage: true, command: 'cat', ttyEnabled: true)
   ])
 {
-  node(POD_LABEL) {
-    stage('Get versions') {
-      container('all-in-one') {
-        stage('checking') {
-          sh 'mvn -version'
+    node(POD_LABEL) {
+        stage('Buid') {
+            container('all-in-one') {
+              sh "docker build -t myapp:latest ."
+              sh "docker images"
+            }
         }
-      }
     }
-  }
 }
