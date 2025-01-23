45104504d4217bec5702e0298f084f3c4d71d341
naivedh <naivedh@gmail.com>
2025-01-23 14:04:57 +0530

    1

---
+++ text.groovy
@@ -0,0 +1,19 @@
+podTemplate(
+  agentContainer: 'jnlp',
+  agentInjection: true,
+  showRawYaml: false,
+  containers: [
+    containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:latest', alwaysPullImage: true, privileged: true),
+    containerTemplate(name: 'all-in-one', image: 'naivedh/jenkins-agent-all-in-one:latest', command: 'cat', ttyEnabled: true)
+  ])
+{
+  node(POD_LABEL) {
+    stage('Get versions') {
+      container('all-in-one') {
+        stage('checking') {
+          sh 'mvn -version'
+        }
+      }
+    }
+  }
+}
